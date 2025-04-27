45a82be0216470354c107e922c47b770419d06b3
System <dimasoft976@gmail.com>
2025-04-27 15:46:08 +0300

    Обновил __init__.py

--- pycc/__init__.py
+++ pycc/__init__.py
@@ -2,6 +2,7 @@ from .lexer import *
 from .parser import *
 from .compiler import *
 from .vm import *
+from .cpp import *
 import sys
 
 def run_c(program):
@@ -9,7 +10,7 @@ def run_c(program):
 
     compiler = Compiler()
 
-    ast = parser.parse(c_lexer(program))
+    ast = parser.parse(c_lexer(preproc(program)))
     bytecode = compiler.compileast(ast)
 
     vm = VirtualMachine()
