d400b5ec4dc3725562efd92cf5f28c40d803659b
System <dimasoft976@gmail.com>
2025-05-17 13:34:33 +0300

    Create stdlib.h

---
+++ pycc/libc/stdlib.h
@@ -0,0 +1,19 @@
+//инкремент
+void inc(x) {
+  x = x + 1;
+}
+
+//декремент
+void dec(x) {
+  x = x - 1;
+}
+
+//добавить
+void add(l, r) {
+  int res = l + r;
+}
+
+//отнять
+void sub(l, r) {
+  int res = l - r;
+}
